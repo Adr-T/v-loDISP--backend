02cc0bad1e2f590709ee763745656d439864ee71
Adrien <ad.tranchand@gmail.com>
2024-08-07 18:46:15 +0200

    route/bikes commit v2

--- routes/bikes.js
+++ routes/bikes.js
@@ -2,48 +2,55 @@ var express = require("express");
 var router = express.Router();
 
 router.get("/", async (req, res) => {
-    let velibVeloAavailable;
-    let velibParkingAavailable;
-    let limVeloAvailable;
+    // let velibVeloAavailable;
+    // let velibParkingAavailable;
+    // let limVeloAvailable;
 
-    await fetch(
-        `https://velib-metropole-opendata.smovengo.cloud/opendata/Velib_Metropole/station_status.json`
-    )
-        .then((response) => response.json())
-        .then((data) => {
-            if (data) {
-                velibVeloAavailable = data.data.stations;
-            } else {
-                res.json({ veloAavailable: "not found" });
-            }
+    const [resVelib1, resVelib2, resLime] = await Promise.all([
+        fetch(
+            "https://velib-metropole-opendata.smovengo.cloud/opendata/Velib_Metropole/station_status.json"
+        ),
+        fetch(
+            "https://velib-metropole-opendata.smovengo.cloud/opendata/Velib_Metropole/station_information.json"
+        ),
+        fetch(
+            "https://data.lime.bike/api/partners/v2/gbfs/paris/free_bike_status"
+        ),
+    ]);
+
+    const [dataVelib1, dataVelib2, dataLime] = await Promise.all([
+        resVelib1.json(),
+        resVelib2.json(),
+        resLime.json(),
+    ]);
+
+    const velibStationMap = new Map();
+
+    for (const d of dataVelib2.data.stations) {
+        velibStationMap.set(d.station_id, {
+            latitude: d.lat,
+            longitude: d.lon,
         });
-    await fetch(
-        `https://velib-metropole-opendata.smovengo.cloud/opendata/Velib_Metropole/station_information.json`
-    )
-        .then((response) => response.json())
-        .then((data) => {
-            if (data) {
-                velibParkingAavailable = data.data.stations;
-            } else {
-                res.json({ parkingAavailable: "not found" });
-            }
-        });
-    await fetch(
-        `https://data.lime.bike/api/partners/v2/gbfs/paris/free_bike_status`
-    )
-        .then((response) => response.json())
-        .then((data) => {
-            if (data) {
-                limVeloAvailable = data.data.bikes;
-            } else {
-                res.json({ parkingAavailable: "not found" });
-            }
-        });
-    await res.json({
-        velibParkingAavailable,
-        velibVeloAavailable,
-        limVeloAvailable,
+    }
+
+    const velibData = dataVelib1.data.stations.map((e) => {
+        const coords = velibStationMap.get(e.station_id);
+        return {
+            stationId: e.station_id,
+            bikesAvailable: e.num_bikes_available,
+            ...coords,
+        };
     });
+
+    const limeData = dataLime.data.bikes.map((e) => {
+        return {
+            bikeId: e.bike_id,
+            latitude: e.lat,
+            longitude: e.lon,
+        };
+    });
+
+    res.json({ velibData, limeData });
 });
 
 module.exports = router;
