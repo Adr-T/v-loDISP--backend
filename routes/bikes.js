9eddd16200c8b363da4a23195b7bef934a8c6559
Adrien <ad.tranchand@gmail.com>
2024-08-09 15:00:19 +0200

    backend route bikes new setup + module boundingBox

--- routes/bikes.js
+++ routes/bikes.js
@@ -1,11 +1,21 @@
 var express = require("express"); // Création d'un routeur express
 var router = express.Router();
 
+const { calculateBoundingBox } = require("../modules/boundingBox");
+
 // Définition d'une route GET à la racine du routeur
-router.get("/", async (req, res) => {
+router.get("/:lat/:lon", async (req, res) => {
     // let velibVeloAavailable;
     // let velibParkingAavailable;
     // let limVeloAvailable;
+
+    const { lat, lon } = req.params;
+
+    const userBoundingBox = calculateBoundingBox(
+        parseFloat(lat),
+        parseFloat(lon),
+        300
+    );
 
     // Envoi de requêtes pour récupérer les données des stations Velib et Lime en parallèle
     const [resVelib1, resVelib2, resLime] = await Promise.all([
@@ -32,30 +42,49 @@ router.get("/", async (req, res) => {
 
     // Remplissage de la carte avec les coordonnées des stations Velib
     for (const d of dataVelib2.data.stations) {
-        velibStationMap.set(d.station_id, {
-            latitude: d.lat,
-            longitude: d.lon,
-        });
+        if (
+            d.lat > userBoundingBox.minLat &&
+            d.lat < userBoundingBox.maxLat &&
+            d.lon > userBoundingBox.minLon &&
+            d.lon < userBoundingBox.maxLon
+        ) {
+            velibStationMap.set(d.station_id, {
+                latitude: d.lat,
+                longitude: d.lon,
+            });
+        }
     }
 
+    let velibData = [];
     // Transformation des données des stations Velib pour inclure les informations de disponibilité des vélos et les coordonnées
-    const velibData = dataVelib1.data.stations.map((e) => {
+    for (const e of dataVelib1.data.stations) {
         const coords = velibStationMap.get(e.station_id); // Récupération des coordonnées de la station
-        return {
-            stationId: e.station_id,
-            bikesAvailable: e.num_bikes_available,
-            ...coords, // Ajout des coordonnées au résultat
-        };
-    });
+        if (coords) {
+            velibData.push({
+                stationId: e.station_id,
+                bikesAvailable: e.num_bikes_available,
+                ...coords, // Ajout des coordonnées au résultat
+            });
+        }
+    }
 
     // Transformation des données des vélos Lime pour inclure l'ID et les coordonnées des vélos
-    const limeData = dataLime.data.bikes.map((e) => {
-        return {
-            bikeId: e.bike_id,
-            latitude: e.lat,
-            longitude: e.lon,
-        };
-    });
+    let limeData = [];
+
+    for (const d of dataLime.data.bikes) {
+        if (
+            d.lat > userBoundingBox.minLat &&
+            d.lat < userBoundingBox.maxLat &&
+            d.lon > userBoundingBox.minLon &&
+            d.lon < userBoundingBox.maxLon
+        ) {
+            limeData.push({
+                bikeId: d.bike_id,
+                latitude: d.lat,
+                longitude: d.lon,
+            });
+        }
+    }
 
     // Envoi de la réponse en JSON avec les données des vélos Velib et Lime
     res.json({ velibData, limeData });
